ce532e67fbf173783dc5b29537493126c89ecb2a
rqfaelviana <rafael.lucas111215@gmail.com>
2025-10-28 17:15:13 -0300

    Protege a SECRET_KEY com .env e decouple

--- configs/settings.py
+++ configs/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-79w2w2+$4m2&&!ao+i9!v5@s+i+q#66uho)m$cl)&-jh*g29tv'
+#A secret key pode ser lida no arquivo .env
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
